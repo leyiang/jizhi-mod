d36f1b7e5d6271418a37714d015dbdc3cde44c4a
Yiang <yiangforwork@gmail.com>
2025-01-05 07:18:16 +0800

    update: remove used code && set title

--- pages/newtab/App.jsx
+++ pages/newtab/App.jsx
@@ -1,11 +1,9 @@
 import { DARK_THEME, FONTNAME_LIST, LIGHT_THEME, POEM_MAXLINELENGTH } from "../components/constants";
 import { getRandomPoem } from "../components/poems";
 import { useEffect, useState, useRef, useCallback, useMemo } from "react";
-import { browser } from "wxt/browser";
 import { IoMoonOutline as MoonIcon, IoSunnyOutline as SunIcon } from "react-icons/io5";
 import { MdTimelapse as SyncIcon } from "react-icons/md";
 import { BiFontFamily as FontIcon } from "react-icons/bi";
-import { IoVolumeHighOutline as VolumeOnIcon, IoVolumeMuteOutline as VolumeOffIcon } from "react-icons/io5";
 import "animate.css";
 import "./App.css";
 
@@ -15,9 +13,6 @@ export default function App() {
   const [fontIndex, setFontIndex] = useState(() => parseInt(localStorage.getItem("fontIndex") || "0", 10));
   const [poem, setPoem] = useState(() => getRandomPoem());
   const [isAnimating, setIsAnimating] = useState(true);
-  const [voiceData, setVoiceData] = useState(null);
-  const [isMuted, setIsMuted] = useState(() => JSON.parse(localStorage.getItem("isMuted") || "false"));
-  const audioRef = useRef(new Audio());
 
   const mediaHandleChange = useCallback((event) => {
     const storedTheme = localStorage.getItem("theme") || "sync";
@@ -67,38 +62,6 @@ export default function App() {
     return () => clearTimeout(timer);
   }, [poem.title]);
 
-  useEffect(() => {
-    document.title = navigator.languages.includes("zh") ? "新标签页" : "New Tab";
-  }, []);
-
-  useEffect(() => {
-    audioRef.current.muted = isMuted;
-    localStorage.setItem("isMuted", JSON.stringify(isMuted));
-  }, [isMuted]);
-
-  const playVoice = useCallback(async () => {
-    if (isMuted) return;
-
-    try {
-      if (!voiceData) {
-        const response = await browser.runtime.sendMessage({ action: "getVoice", text: poem.title });
-        if (!response.url) throw new Error(response.error || "获取语音数据失败");
-
-        setVoiceData(response.url);
-        audioRef.current.src = response.url;
-      } else {
-        audioRef.current.currentTime = 0;
-      }
-
-      await audioRef.current.play();
-    } catch (error) {
-      console.error("播放音频时出错:", error.message);
-      // 这里可以添加用户友好的错误提示，比如使用 toast 通知
-    }
-  }, [isMuted, poem.title, voiceData]);
-
-  const toggleMute = useCallback(() => setIsMuted((prevMuted) => !prevMuted), []);
-
   const toggleTheme = useCallback(() => {
     setTheme((prevTheme) => ["light", "dark", "sync"][(["light", "dark", "sync"].indexOf(prevTheme) + 1) % 3]);
   }, []);
@@ -135,7 +98,6 @@ export default function App() {
             <p
               id="poem-title-container"
               className="text-5xl mb-10 whitespace-pre-wrap cursor-pointer transition-all duration-300 hover:scale-105"
-              onClick={playVoice}
             >
               {poem.title}
             </p>
@@ -180,16 +142,6 @@ export default function App() {
               <input type="checkbox" onClick={toggleFont} />
               <FontIcon className="swap-on fill-current w-8 h-8" />
               <FontIcon className="swap-off fill-current w-8 h-8" />
-            </label>
-          </div>
-        </div>
-        <div className="ml-4"></div>
-        <div className="tooltip" data-tip="静音">
-          <div id="font-toggle" className="custom-settings-button-style transition-all duration-300 hover:scale-110">
-            <label className="swap">
-              <input type="checkbox" checked={isMuted} onChange={toggleMute} />
-              <VolumeOffIcon className="swap-on fill-current w-8 h-8" />
-              <VolumeOnIcon className="swap-off fill-current w-8 h-8" />
             </label>
           </div>
         </div>
