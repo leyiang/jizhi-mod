f692ab39a5bfd3321462850e61b9521544036cfa
Yiang <yiangforwork@gmail.com>
2025-01-11 12:22:15 +0800

    update: inject vimiumc
    https://github.com/gdh1995/vimium-c/wiki/Inject-into-other-extensions#modify-html-pages-of-the-extension
    INject vimiumc to use the shortucts on new tab page.

--- wxt.config.js
+++ wxt.config.js
@@ -5,8 +5,12 @@ export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   vite: () => ({}),
   manifest: {
+	manifest_version: 2,
     author: "xxnuo",
-    name: "浮生梦",
+    name: "浮生梦(注入VimiumC)",
     description: "支持自定义新标签页的 Chrome 扩展，在新标签页上展示中国经典诗词。",
+
+	// 注入Vimium C
+	content_security_policy: "script-src 'self' chrome-extension://hfjbmagddngcpeloejdejnfgbamkjaeg/; object-src 'self'",
   },
 });
